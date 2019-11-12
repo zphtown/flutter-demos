5c585e5ffde66b01af89f6e1f78332ef5050daf0
zphtown@163.com <zphtown@163.com>
2019-11-12 18:51:26 +0800

    '12'

--- zph_travel/lib/pages/home_page.dart
+++ zph_travel/lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +7,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var _images = [
+      'http://via.placeholder.com/350x150',
+      'http://via.placeholder.com/350x150',
+      'http://via.placeholder.com/350x150',
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Text('home')
+    return ListView(
+        children: <Widget>[
+            Container(
+                height: 160,
+                child: Swiper(
+                    itemBuilder: (BuildContext context,int index){
+                        return new Image.network(image: _images[index], placeholder: 'imgs/img1.jpg',fit: BoxFit.fill);
+                    },
+                    itemCount: _images.length,
+                    pagination: new SwiperPagination(),
+                    control: new SwiperControl(),
+                )
+            )
+        ],
     );
   }
 }
